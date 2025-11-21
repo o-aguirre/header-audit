73429a9a31b48cec53fd438d9c2fb684ffaa4ecc
o-aguirre <two.aguirre@gmail.com>
2025-11-21 13:09:59 -0300

    add controller

---
+++ back/src/main/java/header_audit/back/controller/SecurityAnalyzerController.java
@@ -0,0 +1,19 @@
+package header_audit.back.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1/security-analyzer")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityAnalyzerController {
+
+    
+
+}
